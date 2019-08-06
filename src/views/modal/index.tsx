6a8f8b06fba120f01badf2aa23a3d47bb73c1227
terry <397246977@qq.com>
2019-08-06 18:03:53 +0800

    表格中加入气泡

--- src/views/modal/index.tsx
+++ src/views/modal/index.tsx
@@ -4,94 +4,94 @@ import {Modal, Button} from 'antd';
 import {modalVisibleHandle} from '../../redux/action'
 
 const easyStyle = {
-        display: 'flex',
-        "justifyContent": "space-around"
+    display: 'flex',
+    "justifyContent": "space-around"
+};
+
+const deleteBaseOpt = {
+    title: `是否永久删除?`,
+    okText: `确认`,
+    cancelText: `取消`,
+    centered: true
+};
+
+export class CommonModal extends React.Component < any,
+any > {
+    state = {
+        name: "111"
+    }
+    // 基础函数弹窗
+    private showTipOfBase = () => {
+        Modal.confirm({
+            ...deleteBaseOpt
+        });
+    }
+    // 无提示图标
+    private showTipOfNotIcon = () => {
+        Modal.confirm({
+            ...deleteBaseOpt,
+            icon: null
+        });
+    }
+    // 函数弹窗按钮带图标
+    private showTipOfBtnIcon = () => {
+        Modal.confirm({
+            ...deleteBaseOpt,
+            okButtonProps: {
+                icon: "check-circle",
+                type: "ghost"
+            },
+            cancelButtonProps: {
+                icon: "close-circle",
+                type: "ghost"
+            }
+        })
+    }
+    // 图片预览
+    private previewImg = () => {
+        let random = Math.random() * 10;
+        let imgUrl = "";
+        if (random <= 5) {
+            imgUrl = require('../../asset/img/1.jpg')
+        } else {
+            imgUrl = require('../../asset/img/2.jpg')
+        }
+        Modal.confirm({className: "modal-img-preview", centered: true, maskClosable: true, content: (<img src={`${imgUrl}`}/>)})
     }
 
-    const deleteBaseOpt = {
-            title: `是否永久删除?`,
-            okText: `确认`,
-            cancelText: `取消`,
-            centered: true
-        }
+    private input = () => {
+        let {dispatch} = this.props;
+        console.log(this.props);
+        Modal.info({
+            content: `${ "store"}`,
+            onOk: () => {
+                dispatch(modalVisibleHandle({
+                    visible: true
+                }, true))
+            }
+        })
+    }
+    private output = () => {
+        console.log(this.props);
+        Modal.info({content: `${ "store"}`})
+    }
+    render() {
+        return (
+            <div>
+                <div>
+                    函数弹窗
+                </div>
+                <div style={easyStyle}>
+                    <Button onClick={this.showTipOfBase}>基础弹窗</Button>
+                    <Button onClick={this.showTipOfNotIcon}>无提示图标</Button>
+                    <Button onClick={this.showTipOfBtnIcon}>按钮带图标</Button>
+                    <Button onClick={this.previewImg}>图片预览</Button>
+                    <Button onClick={this.input}>输入state</Button>
+                    <Button onClick={this.output}>输出state</Button>
+                </div>
+            </div>
+        )
+    }
+}
 
-        export class CommonModal extends React.Component < any,
-        any > {
-            state = {
-                name: "111"
-            }
-            // 基础函数弹窗
-            private showTipOfBase = () => {
-                Modal.confirm({
-                    ...deleteBaseOpt
-                });
-            }
-            // 无提示图标
-            private showTipOfNotIcon = () => {
-                Modal.confirm({
-                    ...deleteBaseOpt,
-                    icon: null
-                });
-            }
-            // 函数弹窗按钮带图标
-            private showTipOfBtnIcon = () => {
-                Modal.confirm({
-                    ...deleteBaseOpt,
-                    okButtonProps: {
-                        icon: "check-circle",
-                        type: "ghost"
-                    },
-                    cancelButtonProps: {
-                        icon: "close-circle",
-                        type: "ghost"
-                    }
-                })
-            }
-            // 图片预览
-            private previewImg = () => {
-                let random = Math.random() * 10;
-                let imgUrl = "";
-                if (random <= 5) {
-                    imgUrl = require('../../asset/img/1.jpg')
-                } else {
-                    imgUrl = require('../../asset/img/2.jpg')
-                }
-                Modal.confirm({className: "modal-img-preview", centered: true, maskClosable: true, content: (<img src={`${imgUrl}`}/>)})
-            }
-
-            private input = () => {
-                let {dispatch} = this.props;
-                console.log(this.props);
-                Modal.info({
-                    content: `${ "store"}`,
-                    onOk: () => {
-                        dispatch(modalVisibleHandle({
-                            visible: true
-                        }, true))
-                    }
-                })
-            }
-            private output = () => {
-                console.log(this.props);
-                Modal.info({content: `${ "store"}`})
-            }
-            render() {
-                return (
-                    <div>
-                        <div>
-                            函数弹窗
-                        </div>
-                        <div style={easyStyle}>
-                            <Button onClick={this.showTipOfBase}>基础弹窗</Button>
-                            <Button onClick={this.showTipOfNotIcon}>无提示图标</Button>
-                            <Button onClick={this.showTipOfBtnIcon}>按钮带图标</Button>
-                            <Button onClick={this.previewImg}>图片预览</Button>
-                            <Button onClick={this.input}>输入state</Button>
-                            <Button onClick={this.output}>输出state</Button>
-                        </div>
-                    </div>
-                )
-            }
-        }
-
-        export default connect(store => store)(CommonModal);
+export default connect(store => store)(CommonModal);
--- src/views/table/baseTable.tsx
+++ src/views/table/baseTable.tsx
@@ -1,18 +1,47 @@
 import * as React from 'react';
-import { Table } from 'antd';
-import { connect } from 'react-redux';
+import {Table, Popover} from 'antd';
+import {connect} from 'react-redux';
 
-class BaseTable extends React.Component<any,any>{
+class BaseTable extends React.Component < any,
+any > {
 
-    componentDidMount(){
+    componentDidMount() {
         console.log(this.props)
     }
 
-    render(){
+    private columns : any = [
+        {
+            title: "序号",
+            key: "idx"
+        }, {
+            title: "姓名",
+            dataIndex: "name"
+        }, {
+            title: "城市",
+            render: (text : any) => {
+                let cityList = text
+                    .city
+                    .join("、")
+                return (
+                    <Popover content={cityList} style={{width:`120px`}} overlayClassName="custom-tip">
+                        <div className="table-td-item">{cityList}</div>
+                    </Popover>
+                )
+            }
+        }
+    ];
+
+    private dataSource:any = [
+        {name:"张三",key:1,city:["全国"]},
+        {name:"张三",key:2,city:["河北省","河南省"]},
+        {name:"张三",key:3,city:["河北省","广西壮族自治区","临汾","临淄","威海","济南"]}
+    ];
+
+    render() {
         return (
-            <div>
+            <div className="base-table-module">
                 base table
-                <Table bordered/>
+                <Table bordered columns={this.columns} dataSource={this.dataSource}/>
             </div>
         )
     }
